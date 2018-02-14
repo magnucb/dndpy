866d7fc271f578e988344f303b7a36e11b879a31
(/)yvind <o.b.svendsen@astro.uio.no>
2018-02-14 12:01:42 +0100

    Add data-folder for DnD-data and appropriate tools

---
+++ data/README.md
@@ -0,0 +1,4 @@
+Data-folder
+===========
+
+Contains Dnd-data-files, and the appropriate reading tools
---
+++ data/datafile.py
@@ -0,0 +1,24 @@
+"""
+This file holds classes for handling DnD-datafiles.
+"""
+
+class DataFile(object):
+    """
+    Instance of this class opens a file, reads content as single string,
+    and closes file.
+    Sorting content-string into useful data is left to other classes.
+    """
+    def __init__(self, filename):
+        self.filename = filename
+        self.content = self.read_file()
+
+    def read_file(self, filename):
+        """Return data from self.filename as string"""
+        with open(self.filename, 'r') as infile:
+            content_string = infile.read()
+        return str(content_string)
+
+    def write_file(self):
+        """Rewrite self.content to self.filename"""
+        with open(self.filename, 'w') as outfile:
+            outfile.write(self.content)
